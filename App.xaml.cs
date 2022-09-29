4cfca9d964ba300cfe9c72a388c5260afbc70be7
nor0x <nor0x@users.noreply.github.com>
2022-09-29 16:37:13 +0200

    macos added

--- App.xaml.cs
+++ App.xaml.cs
@@ -14,7 +14,7 @@ public partial class App : Application
         AppCenter.Start("ios=14640cf6-a71f-4d11-b54f-5fbc87cb1a25;" +
                   "windowsdesktop=bc07e88d-183e-491c-bf5a-7be0eef931fa;" +
                   "android=427fca12-c72f-470b-8528-44f1155e5f56;" +
-                  "macos={Your macOS App secret here};",
+                  "macos=d1ed7021-2687-4e13-80ff-35d7f6015c74;",
                   typeof(Analytics), typeof(Crashes));
 
         MainPage = new AppShell();
