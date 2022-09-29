7af4b47907b992481a8a845075ce98a15e641fdf
Johnny <nor0x@users.noreply.github.com>
2022-09-29 16:32:18 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace MauiAppCenter;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+        //these are just for my test app - no real secrets here
+        AppCenter.Start("ios=14640cf6-a71f-4d11-b54f-5fbc87cb1a25;" +
+                  "windowsdesktop=bc07e88d-183e-491c-bf5a-7be0eef931fa;" +
+                  "android=427fca12-c72f-470b-8528-44f1155e5f56;" +
+                  "macos={Your macOS App secret here};",
+                  typeof(Analytics), typeof(Crashes));
+
+        MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace MauiAppCenter;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		Analytics.TrackEvent("Counter clicked", new Dictionary<string, string> { { "Count", count.ToString() } });
+    }
+
+	private void OnExceptionClicked(object sender, EventArgs e)
+	{
+        //simulate exception
+        Crashes.TrackError(new Exception("Exception thrown from button click"));
+    }
+}
+
